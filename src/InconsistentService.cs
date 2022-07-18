cedecd670d954485f37ab569b7323501ef5e6510
Xavier <inscription@mobius.zone>
2022-07-18 22:31:06 +0200

    Coding styles

--- src/InconsistentService.cs
+++ src/InconsistentService.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection;
+
+public class InconsistentService
 {
-    public class InconsistentService
-    {
-        public Type ServiceType { get; set; }
-        public ServiceLifetime ServiceLifeTime { get; set; }
-        public Type CallingServiceType { get; set; }
-        public ServiceLifetime CallingServiceLifeTime { get; set; }
-    }
+    public Type ServiceType { get; set; }
+    public ServiceLifetime ServiceLifeTime { get; set; }
+    public Type CallingServiceType { get; set; }
+    public ServiceLifetime CallingServiceLifeTime { get; set; }
 }
--- src/InconsistentServicesException.cs
+++ src/InconsistentServicesException.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection;
+
+[Serializable]
+public class InconsistentServicesException : Exception
 {
-    [Serializable]
-    public class InconsistentServicesException : Exception
+    public InconsistentService[] InsaneServices { get; }
+    public int ErrorCount => InsaneServices.Length;
+    public InconsistentServicesException(params InconsistentService[] insaneServices)
+        : this($"One or more services are not in a valid state. See {nameof(InconsistentService)} property for errors descriptions", insaneServices)
     {
-        public InconsistentService[] InsaneServices { get; }
-        public int ErrorCount => InsaneServices.Length;
-        public InconsistentServicesException(params InconsistentService[] insaneServices)
-            : this($"One or more services are not in a valid state. See {nameof(InconsistentService)} property for errors descriptions", insaneServices)
-        {
-            InsaneServices = insaneServices;
-        }
-        public InconsistentServicesException(string message, params InconsistentService[] insaneServices)
-            : base(message)
-        {
-            InsaneServices = insaneServices;
-        }
+        InsaneServices = insaneServices;
     }
+    public InconsistentServicesException(string message, params InconsistentService[] insaneServices)
+        : base(message)
+    {
+        InsaneServices = insaneServices;
+    }
+    protected InconsistentServicesException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 }
